01b382f98d0c50680d269eb0290ba033d3aaeeed
Keerthi-Badri <badrikeerthi18@gmail.com>
2024-11-11 16:45:31 +0530

    Day13 AdminProduct

---
+++ Project/store-client/src/components/Admin/AdminTopBar.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+const AdminTopBar = () => {
+  return (
+    <div className='w-full h-full shadow-purple-400 shadow-sm flex justify-end items-center px-4'>AdminTopBar</div>
+  )
+}
+export default AdminTopBar
